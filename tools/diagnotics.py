a8ade564ca1e01a5efdf8f220a2bd9c30ceec402
cindeem <cindee@berkeley.edu>
2013-01-08 13:43:24 -0800

    NF: adding holder for tools to examine possible issues artifacts in data

---
+++ tools/diagnotics.py
@@ -0,0 +1,13 @@
+
+
+
+def chi_sq_movement(groupa, groupb):
+    """ examine if their are significant displacement
+    differences between two groups"""
+    pass
+
+
+def movement_withx():
+    """ check for correlation between displacement and
+    variable of interest"""
+    pass
