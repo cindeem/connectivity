d80cf5a2b8a7d8a24858d84b30bb7e08eba5e485
jelman <jelman@lbl.gov>
2013-01-14 14:16:17 -0800

    Input files can now be specified in dualregess_userscript_example.py

--- scripts/dualregress_userscript_example.py
+++ scripts/dualregress_userscript_example.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import os, sys
 from glob import glob
 sys.path.insert(0, '/home/jagust/jelman/rsfmri_ica/code/connectivity/ica')
@@ -6,36 +7,47 @@ import datetime
 
 if __name__ == '__main__':
 
-    #location of ICA data
-    #######################
-    basedir = '/home/jagust/jelman/rsfmri_ica/data/Test'
+    ###Set paths and specify ICA/template data
+    #########################################################
+    basedir = '/home/jagust/jelman/rsfmri_ica/data'
+
     # output directory
     outdir = os.path.join(basedir, 'OldICA_IC25_ecat.gica', 'dual_regress')
     if os.path.isdir(outdir)==False:
         os.mkdir(outdir)      
     else:
-        #If outdir exists, rename existing dir with timestamp appended
+        #If outdir exists, rename existing outdirdir with timestamp appended
         mtime = os.path.getmtime(outdir)
         tmestamp = datetime.datetime.fromtimestamp(mtime).strftime('%Y-%m-%d_%H-%M-%S')
         newdir = '_'.join([outdir,tmestamp])
         os.rename(outdir,newdir)
         os.mkdir(outdir)
         print outdir, 'exists, moving to ', newdir
+
+    # Specify input data filelist, otherwise searches basedir for data
+    if len(sys.argv) ==2:   #If specified, load file as list of infiles
+        args = sys.argv[1]
+        print 'Using data specified in ', args
+        with open(args, 'r') as f:
+            infiles = f.read().splitlines()
+    else:    #If no infile given, search for data
+        print 'No inputs specified, searching for data in ', basedir
+        # look for subjects already registered filtered func ica data
+        globstr = os.path.join(basedir, #data directory
+                                'B*', #subject directory
+                                'func', #functional data directory
+                                'B*.ica', # subject speccific
+                                'reg_standard', # registered to standard
+                                'filtered_func_data.nii.gz') # data
+        infiles = glob(globstr)
+        infiles.sort()
         
     # location for 4D template components (eg melodic_ICA, laird_4D)
     template = os.path.join(basedir, 'OldICA_IC25_ecat.gica',
                              'groupmelodic.ica','melodic_IC.nii.gz')
     num_ics = 25 #Number of ICs in 4d file. Components 0 thru <num_ics> will be merged.
 
-    # look for subjects already registered filtered func ica data
-    globstr = os.path.join(basedir, #data directory
-                            'B*', #subject directory
-                            'func', #functional data directory
-                            'B*.ica', # subject speccific
-                            'reg_standard', # registered to standard
-                            'filtered_func_data.nii.gz') # data
-    infiles = glob(globstr)
-    infiles.sort()
+
 
     # get mask from groupica
     ############################
@@ -69,10 +81,12 @@ if __name__ == '__main__':
         allic = pydr.split_components(stage2_ts, subid, outdir)
 
 
-    ###Concat ics across subjects
+    ###Concat ics across subjects.
+    ###Only merges ICs from gica, 
+    ###not those of confound regressors
     ###############################################
     for cn, item in enumerate(allic[:num_ics]): #Search through ics using last subject's output
-        datadir, ic = os.path.split(item)       #Only merge ics 0 thru <num_ics>
+        datadir, ic = os.path.split(item)       
         subid = pydr.get_subid(item)
         globstr = ic.replace(subid, '*')
         mergefile, subject_order = pydr.merge_components(datadir,
@@ -83,12 +97,3 @@ if __name__ == '__main__':
 
     os.chdir(startdir)
         
-    """randomise stage3
-                    
-    for i in range(len(subd[subd.keys()[0]])):
-        if i == 0:
-            continue
-        items = [x[i] for x in sorted(subd.values())]
-        cmd = sort_maps_randomise(items, mask, perms=500)
-
-    """
