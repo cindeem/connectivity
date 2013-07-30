0e9d28575df4102ff3fc4d6eaffffb840b4668f5
cindeem <cindee@berkeley.edu>
2013-07-30 15:38:49 -0700

    NF: tools for scan duration estimation

--- tools/calc_scan_durations.py
+++ tools/calc_scan_durations.py
@@ -2,18 +2,23 @@
 import os
 import sys
 import argparse
+import numpy as np
 import scipy.special as sspec
 from scipy import interpolate
 from scipy.signal import cubic
 
+
 def spline_dtrend(data, tr = 2):
     """use spline to detrend data"""
+    ## need to subsample timepoints
+    # for accurate low freq detrending
     y = data
-    x = arange(len(data))
-    xx = np.arange(0, len(data), 2)
+    x = np.arange(len(data))
+    xx = np.arange(0, len(data), tr)
     tck = interpolate.splrep(xx, y, s=0)
     ynew = interpolate.splev(xx,tck,der=0)
-    return data - ynew
+    res = data - ynew
+    return res
 
 def demean(data):
     """ remove mean value from a dataset"""
